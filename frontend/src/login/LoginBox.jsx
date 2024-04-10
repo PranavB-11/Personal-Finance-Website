f12754c2cd5cd8b106f2b98a619b3f51d0ec714f
BardiaK <bkoop@berkeley.edu>
2024-04-10 16:39:21 -0700

    I finished the login page. Just need to face some padding and margins

--- frontend/src/login/LoginBox.jsx
+++ frontend/src/login/LoginBox.jsx
@@ -7,7 +7,13 @@ function LoginBox() {
                 <div id="login-box-inner-container">
                     <h1>Welcome</h1>
                     <h2>Please Login</h2>
+                    <input placeholder="Username" class="login-username-box" type="text" />
+                    <input placeholder="Password" class="login-password-box" type="password" />
+                    <button type="button" id="login-google-button">Login with Google</button>
+                    <button type="button" id="login-calnet-button">Login with CalNet</button>
+                    <button type="button" id="login-signup-button">Sign Up</button>
                 </div>
+
             </div>
         </div>
     )
