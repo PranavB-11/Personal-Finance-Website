bc879b9e08f05b639220e35323185619e2b1eae7
Aman Doshi <amandoshi001@gmail.com>
2024-04-10 19:55:47 -0700

    Add login/signup logic

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.post('/login', (req, res) => {
+    console.log("Received data [login] ...")
+    console.log(req.body)
+    res.send(req.body)
+})
+
+app.post('/signup', (req, res) => {
+    console.log("Received data [signup] ...")
+    console.log(req.body)
+    res.send(req.body)
+})
+
 app.listen(port, () => {
-    console.log(`Listening at localhost:${port}`)
+    console.log(`Listening at localhost:${port}/`)
 })
--- frontend/src/login/LoginBox.jsx
+++ frontend/src/login/LoginBox.jsx
@@ -4,16 +4,14 @@ function LoginBox() {
     return (
         <div id="login-box-outer-container">
             <div id="login-box-container">
-                <div id="login-box-inner-container">
+                <form action="http://localhost:3000/login" method="post" id="login-box-inner-container">
                     <h1>Welcome</h1>
                     <h2>Please Login</h2>
                     <input placeholder="Username" class="login-username-box" type="text" />
                     <input placeholder="Password" class="login-password-box" type="password" />
-                    <button type="button" id="login-google-button">Login with Google</button>
-                    <button type="button" id="login-calnet-button">Login with CalNet</button>
-                    <button type="button" id="login-signup-button">Sign Up</button>
-                </div>
-
+                    <input type="submit" value="Login" id="login-button"/>
+                    <button id="signup-button">Signup</button>
+                </form>
             </div>
         </div>
     )
