80201a25f2a109d49b077956eccad37b51d48910
Pranav Banwasi <pranavbanwasi@berkeley.edu>
2024-04-12 01:02:18 -0700

    modals setup, backend required for further implementation

--- frontend/src/dashboard/DashEntry.jsx
+++ frontend/src/dashboard/DashEntry.jsx
@@ -73,8 +73,53 @@ function DashEntry({ name, budget }) {
                             </div>
                             <button className="add-button">Add</button>
                         </div>
-                        
                     )}
+                    {activeButton === 'delete' && (
+                        <div className="modal-delete-options">
+                            <button className="delete-no-button">No</button>
+                            <button className="delete-yes-button">Yes</button>
+                        </div>
+                    )}
+                    {activeButton === 'settings' && (
+                        <div className="modal-form">
+                            <div className="form-group">
+                                <label htmlFor="item-name">Change Section Name:</label>
+                                <input type="text" id="item-name" name="item-name" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="item-amount">Change Budget:</label>
+                                <input type="text" id="item-amount" name="item-amount" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="item-date">Change Date:</label>
+                                <input type="date" id="item-date" name="item-date" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="item-date">Change Frequency:</label>
+                                <input type="text" id="item-frequency" name="item-frequency" />
+                            </div>
+                            <button className="add-button">Confirm</button>
+                        </div>
+                    )}
+                    {activeButton === 'open' && (
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>Item</th>
+                                    <th>Date</th>
+                                    <th>$$$</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><input type="text" placeholder="Item name" /></td>
+                                    <td><input type="date" placeholder="Date" /></td>
+                                    <td><input type="text" placeholder="Amount" /></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    )}
+
                 </div>
             </Modal>
         </div>
